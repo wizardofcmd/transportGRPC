991b44906242b0ca1209f44cad85847bf53120d4
wizarofcmd <sorinnechiforora@gmail.com>
2022-03-10 17:00:09 +0000

    Added additional RPC for checking tickets for specific train route

--- src/main/java/tickets/TicketsService.java
+++ src/main/java/tickets/TicketsService.java
@@ -24,7 +24,32 @@ public class TicketsService extends ticketsImplBase{
 
 	@Override
 	public void checkAvailableTickets(trainRoute request, StreamObserver<ticketsAvailable> responseObserver) {
-
+		String[] seatTypes = {"First Class Tickets Available", "Second Class Tickets Available",
+		"Economy Class Tickets Available"};
+		String route = request.getRoute();
+		
+		for(String i : seatTypes) {
+		
+			int rand_num = (int)(Math.random()* (50 + 1));
+			String tickets = String.valueOf(rand_num);
+			
+			System.out.println(tickets + " " + i);
+			ticketsAvailable.Builder response = ticketsAvailable.newBuilder();
+		
+			response.setTicketDetail(tickets + " " + i + " for Route " + route);
+			responseObserver.onNext(response.build());
+			
+			try {
+				//wait for a second
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		responseObserver.onCompleted();
 	}
 	
 }
